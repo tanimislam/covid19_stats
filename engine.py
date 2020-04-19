d1cf688e0591f7a62c1fb82bb91b33695c742a31
Tanim Islam <islam5@llnl.gov>
2020-04-18 22:37:46 -0700

    lots and lots and lots of changes. Now have GIS style maps of cases and deaths per county for several MSA. And movies.

--- engine.py
+++ engine.py
@@ -1,5 +1,5 @@
 import os, sys, numpy, glob, pylab, tabulate, datetime, pandas, titlecase
-import defaults
+import defaults, engine_geo
 from itertools import chain
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_agg import FigureCanvasAgg
@@ -18,9 +18,12 @@ def _get_stat_line( line ):
         dstring, '%Y-%m-%d' ).date( ),
       'county' : county_name,
       'state' : state_name,
-      'fips' : int( fips ),
+      'fips' : fips,
       'cumulative cases' : cases_cumulative,
       'cumulative death' : death_cumulative }
+
+def _get_fips_county_state( entry ):
+    return ( entry[ 'fips' ], entry[ 'county' ], entry[ 'state' ] )
 
 all_counties_nytimes_covid19_data = list(filter(None,
     map(_get_stat_line,
@@ -32,13 +35,24 @@ all_counties_state = list(map(
     lambda entry: { 'county' : entry[0], 'state' : entry[1] },
     set(map(lambda entry: ( entry['county'], entry['state'] ), all_counties_nytimes_covid19_data ) ) ) )
 
+fips_data_2018 = engine_geo.load_fips_data( )
+
+fips_countystate_dict = dict(map(lambda f_c_s: ( f_c_s[0], {
+    'county' : f_c_s[1], 'state' : f_c_s[2] } ), set(
+        map(_get_fips_county_state, all_counties_nytimes_covid19_data ) ) ) )
+
+def get_county_state( fips ):
+    assert( fips in fips_countystate_dict )
+    return fips_countystate_dict[ fips ]
+
+
 def get_data_county( county_name, state = 'California' ):
   data_by_date = sorted(filter(lambda entry: county_name in entry['county'] and
                                entry['state'] == state, all_counties_nytimes_covid19_data ),
                         key = lambda entry: entry['date'] )
   return data_by_date
 
-def get_summary_demo_data( data = defaults.bay_area_data, doShow = True ):
+def get_incident_data( data = defaults.bay_area_data ):
     prefix = data[ 'prefix' ]
     regionName = data[ 'region name' ]
     counties_and_states = data[ 'counties' ]
@@ -75,6 +89,64 @@ def get_summary_demo_data( data = defaults.bay_area_data, doShow = True ):
     df_cases_deaths_region[ 'days_from_beginning' ] = list(
         map(lambda mydate: ( mydate - min( cases_deaths_region_bydate ) ).days, 
             df_cases_deaths_region.date ) )
+    #
+    ## now get fips data for counties in this collection
+    fips_collection = set(map(lambda entry: entry['fips'], all_data_region))
+    #
+    ## now get the cumulative cases and cumulative deaths by FIPS code
+    cases_deaths_region_byfips_bydate = { }
+    for mydate in all_data_region_bydate:
+        data_mydate = { }
+        fips_excl = fips_collection - set(map(lambda entry: entry['fips'], all_data_region_bydate[ mydate ]))
+        for fips in fips_excl:
+            data_mydate[ fips ] = { 'cumulative cases' : 0, 'cumulative death' : 0 }
+        for entry in all_data_region_bydate[ mydate ]:
+            data_mydate[ entry[ 'fips' ] ] = {
+                'cumulative cases' : entry[ 'cumulative cases' ],
+                'cumulative death' : entry[ 'cumulative death' ] }
+        cases_deaths_region_byfips_bydate[ mydate ] = data_mydate
+    #
+    ## now add cumulative deaths and cases by fips data to the dataframe
+    for fips in sorted(fips_collection):
+        df_cases_deaths_region[ 'cases_%s' % fips ] = list(
+            map(lambda mydate: cases_deaths_region_byfips_bydate[ mydate ][ fips ][ 'cumulative cases' ],
+                sorted( cases_deaths_region_bydate ) ) )
+        df_cases_deaths_region[ 'deaths_%s' % fips ] = list(
+            map(lambda mydate: cases_deaths_region_byfips_bydate[ mydate ][ fips ][ 'cumulative death' ],
+                sorted( cases_deaths_region_bydate ) ) )
+    #
+    ## now calculate the bounding box of this collection of fips data
+    total_bbox = calculate_total_bbox( fips_collection )
+    boundary_dict = dict(map(lambda fips: ( fips, fips_data_2018[ fips ][ 'points' ] ), fips_collection ) )
+    incident_data = {
+        'df' : df_cases_deaths_region, 'bbox' : total_bbox, 'boundaries' : boundary_dict,
+        'last day' : df_cases_deaths_region.days_from_beginning.max( ) }
+    return incident_data
+
+def calculate_total_bbox( fips_collection ):
+    bbox_tot_array = numpy.array(
+        list(map(lambda fips: fips_data_2018[ fips ][ 'bbox' ], fips_collection ) ) )
+    min_lng = bbox_tot_array[:,0].min( )
+    min_lat = bbox_tot_array[:,1].min( )
+    max_lng = bbox_tot_array[:,2].max( )
+    max_lat = bbox_tot_array[:,3].max( )
+    return (min_lng, min_lat, max_lng, max_lat)
+
+def get_maximum_cases( inc_data ):
+    df = inc_data[ 'df' ]
+    max_case_tup = max(map(lambda key: (
+        key.replace('cases_', '').strip( ),
+        df[key].max( ) ), filter(lambda key: key.startswith('cases_'), df ) ),
+                       key = lambda tup: tup[1] )
+    return max_case_tup
+
+def get_summary_demo_data( data = defaults.bay_area_data, doShow = True ):
+    prefix = data[ 'prefix' ]
+    regionName = data[ 'region name' ]
+    counties_and_states = data[ 'counties' ]
+    inc_data = get_incident_data( data )
+    df_cases_deaths_region = inc_data[ 'df' ]
+    #
     first_date = min( df_cases_deaths_region.date )
     last_date = max( df_cases_deaths_region.date )
     #
@@ -124,4 +196,3 @@ def get_summary_demo_data( data = defaults.bay_area_data, doShow = True ):
     #
     ## now SHOW!
     if doShow: pylab.show( )
- 
---
+++ resources/README.md
@@ -0,0 +1,5 @@
+# README RESOURCES
+
+This folder consists of US census shapefile data, taken from [this URL](https://www.census.gov/geographies/mapping-files/time-series/geo/carto-boundary-file.html). The exploded ZIP archive was downloaded on 17 APRIL 2020 from [cb_2018_us_county_500k.zip](https://www2.census.gov/geo/tiger/GENZ2018/shp/cb_2018_us_county_500k.zip).
+
+I also want to find *recent* data that organizes counties (by FIPS code) into metropolitan areas. The only data I have found on the web is this [metro areas from 1999]().
