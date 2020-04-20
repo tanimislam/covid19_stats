aae09e4fc21d3dc6454b4b6b5ca5c0cefafc927f
Tanim Islam <tanim.islam@gmail.com>
2020-04-20 13:37:45 -0700

    BIG CHANGES TO MAKE IT MORE STANDARD, MORE PORTABLE, AND MORE ENHANCED. BRANCH READY TO BE MERGED INTO MASTER.
    Changes to be committed:
    	modified:   .gitignore
    	new file:   covid19_create_movie_or_summary.py
    	deleted:    defaults.py
    	deleted:    demo.py
    	new file:   engine/__init__.py
    	renamed:    autocrop_image.py -> engine/autocrop_image.py
    	renamed:    engine.py -> engine/core.py
    	renamed:    engine_geo.py -> engine/gis.py
    	renamed:    engine_geoviz.py -> engine/viz.py
    	new file:   resources/fips_2018_adj.pkl.gz
    	modified:   resources/msa_2019.pkl.gz
    	modified:   resources/msa_2019_dict.pkl.gz
    	modified:   resources/msa_2019_post.pkl.gz

--- demo.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-
-import os, sys, numpy, engine, defaults, engine_geo
-
-datas = [ defaults.bay_area_data, defaults.richmond_metro_data, defaults.nyc_data,
-         { 'prefix' : 'us', 'region name' : 'All US',
-          'counties' : engine.all_counties_state } ]
-list(map(lambda dat: engine.get_summary_demo_data( data = dat, doShow = False ), datas ) )
