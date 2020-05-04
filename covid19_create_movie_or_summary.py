94d31aad358d7312c4a686d9ce27f6e667d6ea5a
Tanim Islam <tanim.islam@gmail.com>
2020-05-04 14:09:37 -0700

    Big, ugly changes. Not commenting on diffs due solely to indentation changes.
    1. Added in new raw GIS data: added a Mac OS X numbers version, and
    csv version, of the US Census 2018 FIPS county and state codes. These
    come from all-geocodes-v2018.xlsx.
    
    2. Processed data:
    
       a. now ALL US County FIPS codes, not only MSA counties, live in
       resources. The FIPS to county/state mapping are in
       resources/all_2019_fips_cs_dict.pkl.gz. The county/state to FIPS
       mapping are in
       resources/all_2019_cs_fips_dict.pkl.gz.
    
       b. Consequently, deleted resources/msa_2019_fips_cs_dict.pkl.gz and
       resources/msa_2019_cs_fips_dict.pkl.gz.
    
       c. Added in data_states into CSA.
    
    3. engine/core.py:
    
       a. added in CONUS data (same as structure of MSA data
       dictionaries).
    
       b. added in states data (same as structure of MSA data
       dictionaries).
    
       b. replaced '00001' FIPS code for NYC with fips_nyc (which is
       '00001') variable.
    
    4. engine/gis.py:
    
       a. removed postal codes for all US states and territories except
       for Puerto Rico
    
       b. engine.gis.create_and_store_fips_counties_2019: now serialized
       county/state -> FIPS dict, and FIPS -> county/state dict, for ALL
       US state and PR counties. Spits out to
       resources/all_2019_fips_cs_dict.pkl.gz and
       resources/all_2019_cs_fips_dict.pkl.gz.
    
       c. engine.gis.load_fips_counties_data: instantiates dicts
       from resources/all_2019_fips_cs_dict.pkl.gz and
       resources/all_2019_cs_fips_dict.pkl.gz.
    
    5. engine/viz.py:
    
       a. engine.viz.create_and_draw_basemap_smarter: new method to create
       a matplotlib Basemap when extent of projection ~ size of
       earth. Designed to work for CONUS projection map.
    
       b. engine.viz.plot_cases_or_deaths_bycounty: merged functionality
       of engine.viz.plot_cases_bycounty and
       engine.viz.plot_deaths_bycounty (removed previous 2
       methods). Special mode of operation when plotting for CONUS.
    
       c. engine.viz.plot_cases_deaths_region: changes reflected to use
       engine.viz.plot_cases_or_deaths_bycounty, stylistic improvements,
    
       d. engine.viz.get_summary_demo_data: changes reflected to use
       engine.viz.plot_cases_or_deaths_bycounty.
    
    TODO: will have to add in estimated 2019 populations for remaining
    2019 FIPS counties (not in MSA), to get accurate population count for
    states and US.
    
    Changes to be committed:
           modified:   covid-19-data
           modified:   covid19_create_movie_or_summary.py
           new file:   resources/all-geocodes-v2018.csv
           new file:   resources/all-geocodes-v2018.numbers
           new file:   resources/all_2019_cs_fips_dict.pkl.gz
           new file:   resources/all_2019_fips_cs_dict.pkl.gz
           modified:   engine/__init__.py
           modified:   engine/core.py
           modified:   engine/gis.py
           modified:   engine/viz.py
           deleted:    resources/msa_2019_cs_fips_dict.pkl.gz
           deleted:    resources/msa_2019_fips_cs_dict.pkl.gz

--- covid19_create_movie_or_summary.py
+++ covid19_create_movie_or_summary.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-import os, sys, numpy, tabulate, tempfile, warnings
+import os, sys, numpy, tempfile, warnings, tabulate
 from engine import core, viz, get_string_commas_num
 from argparse import ArgumentParser
 
@@ -38,90 +38,97 @@ def _try_continue( ):
   return val_map[ val ]
 
 def main( ):
-  parser = ArgumentParser( )
-  subparsers = parser.add_subparsers( help = 'Choose one of three options: (M) summarizes stats from metros; (m) make a movie of a metro region; and (s) dumps summary plots of last incident date, and cumulative covid-19 stats, of a metro region.', dest = 'choose_option' )
-  parser_showmetros = subparsers.add_parser( 'M', help = 'If chosen, then list all the metropolitan areas through which we can look.' )
-  parser_showmetros.add_argument( '-f', '--format', help = 'Format of the table that displays MSA summary. Default is "simple".',
-                                  type=str, action='store', choices = [ 'simple', 'github', 'rst' ], default = 'simple' )
-  parser_showmetros.add_argument( '--metros', help = 'If chosen, list of selected metros for which to summarize COVID-19 data.',
-                                  type=str, action='store' )
-  #
-  parser_moviemetro = subparsers.add_parser( 'm', help = 'Make a movie of the COVID-19 cases and deaths trend for the specific Metropolitan Statistical Area (MSA).' )
-  parser_moviemetro.add_argument( '-n', '--name', dest='name', type=str, action='store', default = 'bayarea',
-                                  help = 'Make a movie of this metropolitan area. Default is "bayarea"' )
-  parser_moviemetro.add_argument( '-M', '--maxnum', dest='maxnum', type=int, action='store', default = 5000,
+    parser = ArgumentParser( )
+    subparsers = parser.add_subparsers( help = 'Choose one of three options: (M) summarizes stats from metros; (m) make a movie of a metro region; and (s) dumps summary plots of last incident date, and cumulative covid-19 stats, of a metro region.', dest = 'choose_option' )
+    parser_showmetros = subparsers.add_parser( 'M', help = 'If chosen, then list all the metropolitan areas through which we can look.' )
+    parser_showmetros.add_argument( '-f', '--format', help = 'Format of the table that displays MSA summary. Default is "simple".',
+                                   type=str, action='store', choices = [ 'simple', 'github', 'rst' ], default = 'simple' )
+    parser_showmetros.add_argument( '--metros', help = 'If chosen, list of selected metros for which to summarize COVID-19 data.',
+                                   type=str, action='store' )
+    #
+    parser_moviemetro = subparsers.add_parser( 'm', help = 'Make a movie of the COVID-19 cases and deaths trend for the specific Metropolitan Statistical Area (MSA).' )
+    parser_moviemetro.add_argument( '-n', '--name', dest='name', type=str, action='store', default = 'bayarea',
+                                   help = 'Make a movie of this metropolitan area. Default is "bayarea"' )
+    parser_moviemetro.add_argument( '-M', '--maxnum', dest='maxnum', type=int, action='store', default = 5000,
+                                   help = ' '.join([
+                                       'The limit of cases/deaths to visualize.',
+                                       'Default is %s.' % get_string_commas_num( 5000 ),
+                                       'You should use a limit larger (by at least 2, no more than 10) than the maximum number of cases recorded for a county in that MSA.' ]) )
+    parser_moviemetro.add_argument( '--conus', dest='do_conus', action='store_true', default = False,
+                                   help = 'If chosen, then make a movie of the COVID-19 cases and deaths trends for the Continental US (CONUS).' )
+    parser_moviemetro.add_argument(
+        '-y', '--yes', dest='do_yes_moviemetro', action='store_true', default = False,
+        help = 'If chosen, then do not confirm --maxnum.' )
+    #
+    parser_summmetro  = subparsers.add_parser( 's', help = 'Make a summary plot, and incident data file, of COVID-19 cases and deaths trend, for the specific Metropolitan Statistical Area (MSA).' )
+    parser_summmetro.add_argument( '-n', '--name', dest='summmetro_name', type=str, action='store', default = 'bayarea', metavar = 'NAME',
                                   help = ' '.join([
-                                    'The limit of cases/deaths to visualize.',
-                                    'Default is %s.' % get_string_commas_num( 5000 ),
-                                    'You should use a limit larger (by at least 2, no more than 10) than the maximum number of cases recorded for a county in that MSA.' ]) )
-  parser_moviemetro.add_argument(
-    '-y', '--yes', dest='do_yes_moviemetro', action='store_true', default = False,
-    help = 'If chosen, then do not confirm --maxnum.' )
-  #
-  parser_summmetro  = subparsers.add_parser( 's', help = 'Make a summary plot, and incident data file, of COVID-19 cases and deaths trend, for the specific Metropolitan Statistical Area (MSA).' )
-  parser_summmetro.add_argument( '-n', '--name', dest='summmetro_name', type=str, action='store', default = 'bayarea', metavar = 'NAME',
-                                 help = ' '.join([
-                                   'Create a summary plot and incident data file of this metropolitan area.',
-                                   'Default is "bayarea".' ]))
-  parser_summmetro.add_argument( '-M', '--maxnum', dest='summmetro_maxnum', type=int, action='store', default = 5000, metavar = 'MAXNUM',
-                                 help = ' '.join([
-                                   'The limit of cases/deaths to visualize.',
-                                   'Default is %s.' % get_string_commas_num( 5000 ),
-                                   'You should use a limit larger (by at least 2, no more than 10) than the maximum number of cases recorded for a county in that MSA.' ]) )
-  parser_summmetro.add_argument(
-    '-y', '--yes', dest='do_yes_summmetro', action='store_true', default = False,
-    help = 'If chosen, then do not confirm --maxnum.' )
-  args = parser.parse_args( )
-  #
-  ## just show the metros
-  if args.choose_option == 'M':
-    metros = None
-    if args.metros is not None:
-      metros = set(list(map(lambda tok: tok.strip( ), args.metros.split(','))))
-    core.display_tabulated_metros( form = args.format, selected_metros = metros )
-    return
-  if args.choose_option == 'm':
+                                      'Create a summary plot and incident data file of this metropolitan area.',
+                                      'Default is "bayarea".' ]))
+    parser_summmetro.add_argument( '-M', '--maxnum', dest='summmetro_maxnum', type=int, action='store', default = 5000, metavar = 'MAXNUM',
+                                  help = ' '.join([
+                                      'The limit of cases/deaths to visualize.',
+                                      'Default is %s.' % get_string_commas_num( 5000 ),
+                                      'You should use a limit larger (by at least 2, no more than 10) than the maximum number of cases recorded for a county in that MSA.' ]) )
+    parser_summmetro.add_argument( '--conus', dest='do_conus_summmetro', action='store_true', default = False,
+                                  help = 'If chosen, then make a movie of the COVID-19 cases and deaths trends for the Continental US (CONUS).' )
+    parser_summmetro.add_argument(
+        '-y', '--yes', dest='do_yes_summmetro', action='store_true', default = False,
+        help = 'If chosen, then do not confirm --maxnum.' )
     #
-    ## now create the following stuff...
-    msaname = args.name.strip( )
-    if msaname not in core.data_msas_2019:
-      print( 'Error, the chosen MSA name %s not one of the %d defined.' % (
-        msaname, len( data.engine_msas_2019 ) ) )
-      return
-    maxnum = args.maxnum
-    if maxnum < 1:
-      print( 'Error, maximum number for visualization %d < 1.' % maxnum )
-      return
-    data_msa = core.data_msas_2019[ msaname ]
-    _summarize_data( data_msa, maxnum )
-    if args.do_yes_moviemetro: status = args.do_yes_moviemetro
-    else: status = _try_continue( )
-    if status:
-      movie_name = viz.create_summary_movie_frombeginning(
-        data = data_msa, maxnum_colorbar = maxnum )
-      return
-  if args.choose_option == 's':
+    args = parser.parse_args( )
     #
-    ## now create the following stuff...
-    msaname = args.summmetro_name.strip( )
-    if msaname not in core.data_msas_2019:
-      print( 'Error, the chosen MSA name %s not one of the %d defined.' % (
-        msaname, len( core.data_msas_2019 ) ) )
-      return
-    maxnum = args.summmetro_maxnum
-    if maxnum < 1:
-      print( 'Error, maximum number for visualization %d < 1.' % maxnum )
-      return
-    
-    data_msa = core.data_msas_2019[ msaname ]
-    if args.do_yes_summmetro:
-      viz.get_summary_demo_data( data_msa, maxnum_colorbar = maxnum )
-      return
-    _summarize_data( data_msa, maxnum )
-    status = _try_continue( )
-    if status:
-      viz.get_summary_demo_data( data_msa, maxnum_colorbar = maxnum )
-      return
+    ## just show the metros
+    if args.choose_option == 'M':
+        metros = None
+        if args.metros is not None:
+            metros = set(list(map(lambda tok: tok.strip( ), args.metros.split(','))))
+        core.display_tabulated_metros( form = args.format, selected_metros = metros )
+        return
+    if args.choose_option == 'm':
+        #
+        ## now create the following stuff...
+        if not args.do_conus:
+            msaname = args.name.strip( )
+            if msaname not in core.data_msas_2019:
+                print( 'Error, the chosen MSA name %s not one of the %d defined.' % (
+                    msaname, len( data.engine_msas_2019 ) ) )
+                return
+            data = core.data_msas_2019[ msaname ]
+        else: data = core.data_conus
+        maxnum = args.maxnum
+        if maxnum < 1:
+            print( 'Error, maximum number for visualization %d < 1.' % maxnum )
+            return
+        _summarize_data( data, maxnum )
+        if args.do_yes_moviemetro: status = args.do_yes_moviemetro
+        else: status = _try_continue( )
+        if status:
+            movie_name = viz.create_summary_movie_frombeginning(
+                data = data, maxnum_colorbar = maxnum )
+            return
+    if args.choose_option == 's':
+        #
+        ## now create the following stuff...
+        if not args.do_conus_summmetro:
+            msaname = args.summmetro_name.strip( )
+            if msaname not in core.data_msas_2019:
+                print( 'Error, the chosen MSA name %s not one of the %d defined.' % (
+                    msaname, len( core.data_msas_2019 ) ) )
+                return
+            data = core.data_msas_2019[ msaname ]
+        else: data = core.data_conus
+        maxnum = args.summmetro_maxnum
+        if maxnum < 1:
+            print( 'Error, maximum number for visualization %d < 1.' % maxnum )
+            return
+
+        _summarize_data( data, maxnum )
+        if args.do_yes_summmetro: status = args.do_yes_summmetro
+        else: status = _try_continue( )
+        if status:
+            viz.get_summary_demo_data( data, maxnum_colorbar = maxnum )
+            return
             
             
 if __name__ == '__main__':
