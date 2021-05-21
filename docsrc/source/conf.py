93e7c531e33efb1cf02bf1b035e99d35a031a6e4
Tanim Islam <tanim.islam@gmail.com>
2021-05-21 09:29:22 -0700

    doc fix.

--- docsrc/source/conf.py
+++ docsrc/source/conf.py
@@ -63,7 +63,7 @@ intersphinx_mapping = {
     'google-auth' : ( 'https://google-auth.readthedocs.io/en/latest', None ),
     'PyPDF2' : ( 'https://pythonhosted.org/PyPDF2', None ),
     'Pillow' : ( 'https://pillow.readthedocs.io/en/stable', None ),
-    'docutils' : ( 'https://docutils.readthedocs.io/en/sphinx-docs', None ),
+    'docutils' : ( 'https://www.sphinx-doc.org/en/master', None ),
     'matplotlib' : ( 'https://matplotlib.org/stable', None ),
     'mutagen'  : ( 'https://mutagen.readthedocs.io/en/latest', None ),
     'pandas'   : ( 'https://pandas.pydata.org/docs', None ),
